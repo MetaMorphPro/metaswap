5374c1d621e24489bc5895551f682cb389b63839
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2020-09-28 10:21:11 +1000

    WIP

--- contracts/MorphToken.sol
+++ contracts/MorphToken.sol
@@ -5,8 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-// SushiToken with Governance.
-contract SushiToken is ERC20("SushiToken", "SUSHI"), Ownable {
+// MorphToken with Governance.
+contract MorphToken is ERC20("MorphToken", "M"), Ownable {
     /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
